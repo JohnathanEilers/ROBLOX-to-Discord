1339902454642f3e98d714ea6b99349b054c76e3
Johnathan Eilers <johnathanplays@gmail.com>
2017-12-28 19:37:35 -0800

    asdf

--- index.js
+++ index.js
@@ -5,24 +5,17 @@ const client = new Discord.Client();
 const version = '1.0.0'
 const prefix = '!'
 
-// set the port of our application
-// process.env.PORT lets the port be set by Heroku
 const port = process.env.PORT || 5000;
 
-// set the view engine to ejs
 app.set('view engine', 'ejs');
 
-// make express look in the `public` directory for assets (css/js/img)
 app.use(express.static(__dirname + '/public'));
 
-// set the home page route
 app.get('/', (request, response) => {
-    // ejs render automatically looks in the views folder
     response.render('index');
 });
 
 app.listen(port, () => {
-    // will echo 'Our app is running on http://localhost:5000 when run locally'
     console.log('Our app is running on http://localhost:' + port);
 });
 
@@ -32,7 +25,7 @@ client.on('ready', () => {
 
 client.on('message', message => {
   if (message.content === prefix + 'linkaccount ') {
-    message.reply('pong but more updated xd');
+    message.reply('In progress!');
   }
 });
 
@@ -43,3 +36,8 @@ client.on('guildMemberAdd', member => {
 });
 
 client.login('Mzk1NjY0MDcxODM3NDgzMDMx.DSWKbg.0JL1QtoK_a6Q27v-htwcErA0kb0');
+
+var http = require("http");
+setInterval(function() {
+    http.get("http://roblox-to-discord.herokuapp.com");
+}, 300000); // every 5 minutes (300000)
